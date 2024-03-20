89953ce746f7828fce4b93d5fd7e8989a0af5e0f
Arkadiusz Rusinek <arek.rusinek94@gmail.com>
2024-03-20 22:26:41 +0100

    added create community command

---
+++ src/MFS.Application/Features/Communities/CreateCommunity/CreateCommunityCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace MFS.Application.Features.Communities.CreateCommunity;
+
+public record CreateCommunityCommand : IRequest<int>
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
---
+++ src/MFS.Application/Features/Communities/CreateCommunity/CreateCommunityCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using MFS.Core.Entities;
+using MFS.Server.Infrastructure.Interfaces.Repositories;
+
+namespace MFS.Application.Features.Communities.CreateCommunity;
+
+public class CreateCommunityCommandHandler: IRequestHandler<CreateCommunityCommand, int>
+{
+    private readonly ICommunityRepository _communityRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateCommunityCommandHandler(IUnitOfWork unitOfWork, ICommunityRepository communityRepository)
+    {
+        _unitOfWork = unitOfWork;
+        _communityRepository = communityRepository;
+    }
+
+    public async Task<int> Handle(CreateCommunityCommand request, CancellationToken cancellationToken)
+    {
+        var community = new Community
+        {
+            Name = request.Name,
+            Description = request.Description
+        };
+
+        var result = _communityRepository.Create(community);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        return result.Id;
+    }
+}
