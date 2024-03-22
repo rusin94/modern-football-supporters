45c2aebef8ae73bae032a4c5575fa8043c855d7e
Arkadiusz Rusinek <arek.rusinek94@gmail.com>
2024-03-22 14:30:41 +0100

    added delete community command

---
+++ src/MFS.Application/Features/Communities/DeleteCommunity/DeleteCommunityCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace MFS.Application.Features.Communities.DeleteCommunity;
+
+public record DeleteCommunityCommand :IRequest
+{
+    public int Id { get; init; }
+}
---
+++ src/MFS.Application/Features/Communities/DeleteCommunity/DeleteCommunityCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using MFS.Server.Infrastructure.Interfaces.Repositories;
+
+namespace MFS.Application.Features.Communities.DeleteCommunity;
+
+public class DeleteCommunityCommandHandler:IRequestHandler<DeleteCommunityCommand>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly ICommunityRepository _communityRepository;
+
+    public DeleteCommunityCommandHandler(IUnitOfWork unitOfWork, ICommunityRepository communityRepository)
+    {
+        _unitOfWork = unitOfWork;
+        _communityRepository = communityRepository;
+    }
+
+    public async Task Handle(DeleteCommunityCommand request, CancellationToken cancellationToken)
+    {
+        var community = await _communityRepository.GetByIdAsync(request.Id);
+        if (community == null)
+        {
+            return;
+        }
+
+        _communityRepository.Delete(community);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+    }
+}
