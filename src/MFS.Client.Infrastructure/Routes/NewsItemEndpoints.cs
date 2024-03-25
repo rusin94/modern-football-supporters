2ff9d4cbcecb9a2203efcdc3a37d7b0e4fc7481d
Arkadiusz Rusinek <arek.rusinek94@gmail.com>
2024-03-25 21:17:53 +0100

    added news item endpoints

---
+++ src/MFS.Client.Infrastructure/Routes/NewsItemEndpoints.cs
@@ -0,0 +1,10 @@
+﻿namespace MFS.Client.Infrastructure.Routes;
+
+public static class NewsItemEndpoints
+{
+    public static string GetAllNewsItems = "api/newsitem";
+    public static string GetNewsItemById(int id) => $"api/newsitem/{id}";
+    public static string SaveNewsItem = "api/newsitem";
+    public static string UpdateNewsItem = "api/newsitem";
+    public static string DeleteNewsItem(int id) => $"api/newsitem/{id}";
+}
