27cd231bd1ece583d0c6286a203d5ca067a62769
Arkadiusz Rusinek <arek.rusinek94@gmail.com>
2024-04-01 21:37:09 +0200

    added community dto

---
+++ src/MFS.Shared/Dto/Communities/CommunityDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MFS.Shared.Dto.Communities;
+
+public record CommunityDto
+{
+    public int Id { get; init; }
+    public string Name { get; init; }
+    public string Description { get; init; }
+}
