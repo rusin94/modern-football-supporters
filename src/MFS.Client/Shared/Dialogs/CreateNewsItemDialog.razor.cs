e6cdba8dac0d1f2fac25fb36e9c91309018a1dc8
Arkadiusz Rusinek <arek.rusinek94@gmail.com>
2024-04-12 21:43:05 +0200

    added form for adding news item

--- src/MFS.Client/Shared/Dialogs/CreateNewsItemDialog.razor.cs
+++ src/MFS.Client/Shared/Dialogs/CreateNewsItemDialog.razor.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Components;
-using Radzen;
+﻿using MFS.Shared.Dto.NewsItems;
 
 namespace MFS.Client.Shared.Dialogs;
 
 public partial class CreateNewsItemDialog
 {
-    
+    public NewsItemCreateDto NewsItemCreateDto { get; set; }
+
+    public void Submit()
+    {
+
+    }
     
 }
