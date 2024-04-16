e0b170c2c8b2f61ee207179856dd439ad48cd6a9
Arkadiusz Rusinek <arek.rusinek94@gmail.com>
2024-04-16 20:08:35 +0200

    news items fixes

--- src/MFS.API/Controllers/NewsItemController.cs
+++ src/MFS.API/Controllers/NewsItemController.cs
@@ -3,6 +3,7 @@ using MFS.Application.Features.NewsItems.Commands.CreateNewsItem;
 using MFS.Application.Features.NewsItems.Commands.DeleteNewsItem;
 using MFS.Application.Features.NewsItems.Commands.UpdateNewsItem;
 using MFS.Application.Features.NewsItems.Queries.GetNewsItems;
+using MFS.Shared.Dto.NewsItems;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MFS.API.Controllers;
@@ -13,23 +14,23 @@ public class NewsItemController : ApiControllerBase
 {
 
     [HttpPost]
-    public async Task<IActionResult> CreateNewsItem(CreateNewsItemCommand command)
+    public async Task<IActionResult> CreateNewsItem(NewsItemCreateDto dto)
     {
-        var result =  await DispatchAsync(command);
+        var result =  await DispatchAsync(new CreateNewsItemCommand(dto));
         return Ok(result);
     }
 
     [HttpPut]
-    public async Task<IActionResult> UpdateNewsItem(UpdateNewsItemCommand command)
+    public async Task<IActionResult> UpdateNewsItem(NewsItemUpdateDto dto)
     {
-        var result = await DispatchAsync(command);
+        var result = await DispatchAsync(new UpdateNewsItemCommand(dto));
         return Ok(result);
     }
 
-    [HttpDelete]
-    public async Task<IActionResult> DeleteNewsItem(DeleteNewsItemCommand command)
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> DeleteNewsItem(int id)
     {
-        await DispatchAsync(command);
+        await DispatchAsync(new DeleteNewsItemCommand(id));
         return Ok();
     }
 
