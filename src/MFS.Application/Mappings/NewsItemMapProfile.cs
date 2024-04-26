98d5f17361649531b9fb831dde85eeea11a561b9
Arkadiusz Rusinek <arek.rusinek94@gmail.com>
2024-04-26 21:21:58 +0200

    changed mapping profile

--- src/MFS.Application/Mappings/NewsItemMapProfile.cs
+++ src/MFS.Application/Mappings/NewsItemMapProfile.cs
@@ -4,10 +4,20 @@ using MFS.Shared.Dto.NewsItems;
 
 namespace MFS.Application.Mappings;
 
-public class NewsItemMapProfile:Profile
+public class NewsItemMapProfile : Profile
 {
     public NewsItemMapProfile()
     {
-        CreateMap<NewsItem, NewsItemDto>().ReverseMap();
+        CreateMap<NewsItem, NewsItemDto>()
+            .ForMember(x => x.Author,
+                opt => opt.MapFrom(
+                    src => src.Author.Value))
+            .ForMember(x=>x.Header,
+                opt=>opt.MapFrom(
+                    src=>src.Title.Value))
+            .ForMember(x=>x.Content,
+                opt=>opt.MapFrom(
+                                       src=>src.Content.Value))
+            .ReverseMap();
     }
 }
