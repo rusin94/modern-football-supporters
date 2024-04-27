cfb682887ee3fc894aa1577f921745554fbac16e
Arkadiusz Rusinek <arek.rusinek94@gmail.com>
2024-04-27 20:48:43 +0200

    added community mapprofile

---
+++ src/MFS.Application/Mappings/CommunityMapProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MFS.Core.Entities;
+using MFS.Shared.Dto.Communities;
+
+namespace MFS.Application.Mappings;
+
+public class CommunityMapProfile : Profile
+{
+    public CommunityMapProfile()
+    {
+        CreateMap<Community, CommunityDto>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name.Value))
+            .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description.Value));
+    }
+}
