f6697d57219fb391dcb7b95637fdb41384693d70
Arkadiusz Rusinek <arek.rusinek94@gmail.com>
2024-05-21 20:23:43 +0200

    added identity migrtions

--- src/MFS.Server.Persistence/Migrations/20240521182305_Identity.Designer.cs
+++ src/MFS.Server.Persistence/Migrations/20240521182305_Identity.Designer.cs
@@ -4,6 +4,7 @@ using MFS.Server.Persistence.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MFS.Server.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240521182305_Identity")]
+    partial class Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
