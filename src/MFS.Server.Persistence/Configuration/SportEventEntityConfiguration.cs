c6c0f7916999318f4569d8641be1b0c6422194d5
Arkadiusz Rusinek <arek.rusinek94@gmail.com>
2024-06-03 20:06:00 +0200

    added sport event entity configuration

---
+++ src/MFS.Server.Persistence/Configuration/SportEventEntityConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using MFS.Core.Entities;
+using MFS.Core.ValueObjects.Community;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MFS.Server.Persistence.Configuration;
+
+public class SportEventEntityConfiguration : IEntityTypeConfiguration<SportEvent>
+{
+    public void Configure(EntityTypeBuilder<SportEvent> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedOnAdd();
+        builder.Property(x => x.Name)
+            .HasConversion(x => x.Value, x => new Name(x))
+            .IsRequired();
+        builder.Property(x => x.Description)
+            .HasConversion(x => x.Value, x => new Description(x))
+            .IsRequired();
+    }
+}
