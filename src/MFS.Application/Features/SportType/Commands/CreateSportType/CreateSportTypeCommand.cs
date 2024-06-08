49d7c5bc9d0c1e769745faa2b93da33d3dd21aab
Arkadiusz Rusinek <arek.rusinek94@gmail.com>
2024-06-08 21:51:16 +0200

    added sport type create handler

--- src/MFS.Application/Features/SportType/Commands/CreateSportType/CreateSportTypeCommand.cs
+++ src/MFS.Application/Features/SportType/Commands/CreateSportType/CreateSportTypeCommand.cs
@@ -3,6 +3,8 @@ using MFS.Shared.Wrapper;
 
 namespace MFS.Application.Features.SportType.Commands.CreateSportType;
 
-public class CreateSportTypeCommand : IRequest<Result<int>>
+public class CreateSportTypeCommand : IRequest<Result<Guid>>
 {
+    public string Name { get; set; }
+    public string Description { get; set; }
 }
--- src/MFS.Application/Features/SportType/Commands/CreateSportType/CreateSportTypeCommandHandler.cs
+++ src/MFS.Application/Features/SportType/Commands/CreateSportType/CreateSportTypeCommandHandler.cs
@@ -1,8 +1,30 @@
 ﻿using MediatR;
+using MFS.Server.Infrastructure.Interfaces.Repositories;
 using MFS.Shared.Wrapper;
 
 namespace MFS.Application.Features.SportType.Commands.CreateSportType;
 
-public class CreateSportTypeCommandHandler : IRequestHandler<CreateSportTypeCommand, Result<int>>
+public class CreateSportTypeCommandHandler : IRequestHandler<CreateSportTypeCommand, Result<Guid>>
 {
+
+    private readonly ISportTypeRepository _sportTypeRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+
+    public CreateSportTypeCommandHandler(ISportTypeRepository sportTypeRepository, IUnitOfWork unitOfWork)
+    {
+        _sportTypeRepository = sportTypeRepository;
+        _unitOfWork = unitOfWork;
+    }
+    public async Task<Result<Guid>> Handle(CreateSportTypeCommand request, CancellationToken cancellationToken)
+    {
+        var sportType = new Core.Entities.SportType
+        {
+            Name = request.Name,
+            Description = request.Description
+        };
+        _sportTypeRepository.Create(sportType);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        return await Result<Guid>.SuccessAsync(sportType.Id, "Sport Type Created");
+    }
 }
